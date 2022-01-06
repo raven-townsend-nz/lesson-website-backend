ea5bb047a7403fd0092ef7477e960136c861ba81
raven-townsend-nz <raven.townsend.nz@gmail.com>
2022-01-06 23:30:34 +1300

    Remove ssl from db setup

--- functions/config/db.js
+++ functions/config/db.js
@@ -13,11 +13,11 @@ exports.createPool = async function () {
         password: functions.config().env.mariadb_pass,
         database: functions.config().env.mariadb_database,
         port: functions.config().env.mariadb_port || 3306,
-        ssl: {
-            ca: Buffer.from(functions.config().env.ssl_ca, "base64").toString("utf-8"),
-            key: Buffer.from(functions.config().env.ssl_key, "base64").toString("utf-8"),
-            cert: Buffer.from(functions.config().env.ssl_cert, "base64").toString("utf-8")
-        }
+        // ssl: {
+        //     ca: Buffer.from(functions.config().env.ssl_ca, "base64").toString("utf-8"),
+        //     key: Buffer.from(functions.config().env.ssl_key, "base64").toString("utf-8"),
+        //     cert: Buffer.from(functions.config().env.ssl_cert, "base64").toString("utf-8")
+        // }
     });
 };
 
